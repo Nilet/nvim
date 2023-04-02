85fc98485e16b561e78d74e5146ffa4fd2d641c9
Nilet <nilet@tutanota.com>
2023-04-02 14:01:42 -0300

    added remaps for git stuff

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,21 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+local blz_fugitive = vim.api.nvim_create_augroup("blz_fugitive", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+    group = blz_fugitive,
+    pattern = "*",
+    callback = function()
+        if vim.bo.ft ~= "fugitive" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        vim.keymap.set("n", "<leader>p", function()
+            vim.cmd.Git('push')
+        end, opts)
+
+    end,
+})
