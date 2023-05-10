5f3af7a048d6516143a0623c49686ef8ff5380a0
Nilet <nilet@tutanota.com>
2023-05-10 19:46:26 -0300

    bufferline config changes

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,1 +1,14 @@
-require("bufferline").setup{}
+require('bufferline').setup {
+    options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+                local sym = e == "error" and " "
+                    or (e == "warning" and " " or "")
+                s = s .. n .. sym
+            end
+            return s
+        end
+    }
+}
